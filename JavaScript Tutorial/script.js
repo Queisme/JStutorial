96f023a349929211e1d6a9a433d301dea902994b
Queisme <queiskey@gmail.com>
2024-07-20 23:47:42 -0500

    update

--- JavaScript Tutorial/script.js
+++ JavaScript Tutorial/script.js
@@ -1,5 +1,36 @@
-console.error('JavaScript Basics');
-console.error('Numbers and Math');
-console.error('Strings');
-console.error('HTML CSS Review');
-console.error('Console.log');
+// console.error('JavaScript Basics');
+// console.error('Numbers and Math');
+// console.error('Strings');
+// console.error('HTML CSS Review');
+// console.error('Console.log');
+// //alert('Welcome!');
+// console.error('Variables');
+
+// let variable1 = 3;
+// console.log(variable1);
+
+// let calculation = 2 + 2;
+// console.log(calculation);
+// console.log(calculation + 2);
+
+// let result = calculation + 2;
+// console.log(result);
+
+// let message = 'Hello';
+// console.log(message);
+// console.log(';');
+
+// variable1 = 5;
+// console.log(variable1);
+
+// variable1++;
+// console.log(variable1);
+
+let cartQuantity = 0;
+const cartQuant = () => console.log(`Cart Quantity: ${cartQuantity}`);
+
+const addToCart = () => console.log(`Cart Quantity: ${cartQuantity + 1}`);
+const add2 = () => console.log(`Cart Quantity: ${cartQuantity}`);
+const add3 = () => console.log(`Cart Quantity: ${cartQuantity}`);
+
+const reCart = () => console.log(`Cart Quantity: ${cartQuantity}`);
